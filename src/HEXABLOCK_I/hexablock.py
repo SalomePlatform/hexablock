269e936ebe4c5ceb08a74f34672a772f66f81bfc
fps <fps@opencascade.com>
2010-11-25 15:30:33 +0000

    Add a distributive test and update hexablock.py Francis KLOSS

--- src/HEXABLOCK_I/hexablock.py
+++ src/HEXABLOCK_I/hexablock.py
@@ -1,7 +1,7 @@
 # -*- coding: latin-1 -*-
 
-# Francis KLOSS - 2009-2010 - CEA-Saclay, DEN, DM2S, SFME, LGLS, F-91191 Gif-sur-Yvette, France
-# =============================================================================================
+# Francis KLOSS - 2010 - CEA-Saclay, DEN, DM2S, SFME, LGLS, F-91191 Gif-sur-Yvette, France
+# ========================================================================================
 
 import salome
 from HEXABLOCK_ORB import *
@@ -19,18 +19,23 @@ for k in dir(component):
 
 del k
 
-def mesh(name, doc, dim=3):
-    geom = smesh.geompy.MakeBox(0, 0, 0,  1, 1, 1)
-    smesh.geompy.addToStudy(geom, name)
-    mesh = smesh.Mesh(geom)
+def mesh(name, doc, dim=3, container="FactoryServer"):
+    geompy = smesh.geompy
+    #fkl: shape  = doc.getShape()
+    shape  = geompy.MakeBox(0, 0, 0,  1, 1, 1)
+    geompy.addToStudy(shape, name)
+
+    component = salome.lcc.FindOrLoadComponent(container, "SMESH")
+    component.init_smesh(salome.myStudy, geompy.geom)
+    mesh = component.Mesh(shape)
 
     so = "libHexaBlockEngine.so"
 
-    algo = smesh.SMESH._objref_SMESH_Gen.CreateHypothesis(smesh.smesh, "HEXABLOCK_3D", so)
-    mesh.mesh.AddHypothesis(geom, algo)
+    algo = smesh.SMESH._objref_SMESH_Gen.CreateHypothesis(component, "HEXABLOCK_3D", so)
+    mesh.mesh.AddHypothesis(shape, algo)
 
-    hypo = smesh.SMESH._objref_SMESH_Gen.CreateHypothesis(smesh.smesh, "HEXABLOCK_Parameters", so)
-    mesh.mesh.AddHypothesis(geom, hypo)
+    hypo = smesh.SMESH._objref_SMESH_Gen.CreateHypothesis(component, "HEXABLOCK_Parameters", so)
+    mesh.mesh.AddHypothesis(shape, hypo)
 
     hypo.SetDocument(doc)
     hypo.SetDimension(dim)
---
+++ src/TEST_PY/test_distrib.py
@@ -0,0 +1,96 @@
+# -*- coding: latin-1 -*-
+
+# Francis KLOSS - 2010 - CEA-Saclay, DEN, DM2S, SFME, LGLS, F-91191 Gif-sur-Yvette, France
+# ========================================================================================
+
+import geompy
+import hexablock
+
+# Build the geometry to mesh with hexahedra: a sphere
+# ---------------------------------------------------
+
+name = "Sphere"
+
+cx = 0
+cy = 0
+cz = 0
+
+radius = 5
+
+sphere = geompy.MakeSphere(cx, cy, cz, radius)
+
+geompy.addToStudy(sphere, name)
+
+# Add a new document
+# ------------------
+
+doc = hexablock.addDocument()
+
+# fkl: doc.addShape(sphere)
+
+# Build the model of blocks: a spherical grid
+# -------------------------------------------
+
+center = doc.addVertex(cx, cy, cz)
+
+axis_x = doc.addVector(1, 0, 0)
+axis_y = doc.addVector(0, 1, 0)
+axis_z = doc.addVector(0, 0, 1)
+
+#fkl: doc.makeSpherical(center, axis_x, 3, 1.0)
+doc.makeCartesian(center, axis_x, axis_y, axis_z, 5, 4, 3)
+
+# Associate the model of block to the geometry
+# --------------------------------------------
+
+# fkl: to do
+
+# Define group of all hexahedra
+# -----------------------------
+
+group_h = doc.addHexaGroup(name+":hexas")
+for i in xrange(doc.countHexa()):
+    e = doc.getHexa(i)
+    group_h.addElement(e)
+
+# Define group of all quadrangles
+# -------------------------------
+
+group_q = doc.addQuadGroup(name+":quadrangles")
+for i in xrange(doc.countQuad()):
+    e = doc.getQuad(i)
+    group_q.addElement(e)
+
+# Define the laws for discretization
+# ----------------------------------
+
+law = doc.addLaw("Uniform" , 3)
+
+# Set the law on all edges of the model of blocks
+# -----------------------------------------------
+
+for i in xrange(doc.countPropagation()):
+    p = doc.getPropagation(i)
+    p.setLaw(law)
+
+# Generate the hexehadral mesh on FactoryServer
+# ---------------------------------------------
+
+mesh_1 = hexablock.mesh(name, doc)
+
+print "Number of hexaedra   :", mesh_1.NbHexas()
+print "Number of quadrangles:", mesh_1.NbQuadrangles()
+print "Number of segments   :", mesh_1.NbEdges()
+print "Number de nodes      :", mesh_1.NbNodes()
+
+# Generate the hexehadral mesh on FooBar container
+# ------------------------------------------------
+
+container = "FooBar"
+
+mesh_2 = hexablock.mesh(name+":"+container, doc, 3, container)
+
+print "Number of hexaedra   :", mesh_2.NbHexas()
+print "Number of quadrangles:", mesh_2.NbQuadrangles()
+print "Number of segments   :", mesh_2.NbEdges()
+print "Number de nodes      :", mesh_2.NbNodes()
