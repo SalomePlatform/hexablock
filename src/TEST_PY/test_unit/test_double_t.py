9bc74c0ffbc9649cf3e8f9292da097ac2130b665
fps <fps@opencascade.com>
2012-01-06 16:00:50 +0000

    Abu : Tests unitaires

---
+++ src/TEST_PY/test_unit/test_double_t.py
@@ -0,0 +1,148 @@
+# -*- coding: latin-1 -*-
+# Hexa : Creation d'hexaedres 
+
+import hexablock
+import os
+import geompy
+
+#---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8
+
+
+# ======================================================= test_double_t
+def test_double_t () :
+
+    doc  = hexablock.addDocument()
+
+    ori1 = doc.addVertex ( 0, 0, 0)
+    ori2 = doc.addVertex (-5, 0, 5)
+    ori3 = doc.addVertex ( 0, 0, 12)
+    ori4 = doc.addVertex (-5, 0, 17)
+
+    vx   = doc.addVector ( 1 ,0, 0)
+    vz   = doc.addVector ( 0, 0, 1)
+
+    nl1 = 10
+    nl2 = 10
+
+    rsmall = 1
+    rmoy   = 2
+    rbig   = 3
+
+    cyl1  = doc.addCylinder (ori1, vz, rbig, nl1)
+    cyl2  = doc.addCylinder (ori2, vx, rmoy, nl2)
+
+    cyl3  = doc.addCylinder (ori3, vz, rbig, nl1)
+    cyl4  = doc.addCylinder (ori4, vx, rmoy, nl2)
+
+    grid1 = doc.makeCylinders (cyl1, cyl2)
+    grid2 = doc.makeCylinders (cyl4, cyl3)
+
+    nx_int = 0
+    nx_ext = 1
+    cyl_big   = 1
+    cyl_small = 0
+    S_E     = 0
+    S_NE    = 1
+    S_N     = 2
+    S_NW    = 3
+    S_MAXI  = 8
+                  ##    Cyl       i     j     k
+    qb = grid1. getQuadIJ (cyl_big, nx_ext, S_E, 4)
+    qh = grid2. getQuadIJ (cyl_big, nx_ext, S_E, 0) 
+
+    vb0 = grid1.getVertexIJK (cyl_big, 2, S_E,  4)  ## cible
+    vb1 = grid1.getVertexIJK (cyl_big, 2, S_NE, 4)
+    vh0 = grid2.getVertexIJK (cyl_big, 2, S_E,  0)   ## depart
+    vh1 = grid2.getVertexIJK (cyl_big, 2, S_NE, 0)
+
+    hliste = []
+
+    hliste.append (qh)
+    for ny in range (S_MAXI) :
+        if ny != S_E :
+          hliste.append (grid2.getQuadIJ (cyl_big, nx_ext, ny, 0)) 
+
+    for ny in range (4) :
+        hliste.append (grid2.getQuadIJ (cyl_big, nx_int, ny, 0)) 
+
+    hauteur = 3
+    doc.joinQuads  (hliste, qb, vh0, vb0, vh1, vb1, hauteur)
+
+    doc.saveVtk ("double_t.vtk")
+    return doc
+
+# ======================================================= test_lorraine
+def test_lorraine () :
+
+    doc  = hexablock.addDocument()
+
+    ori1 = doc.addVertex ( 0, 0, 0)
+    ori2 = doc.addVertex (-5, 0, 5)
+    ori3 = doc.addVertex ( 0, 0, 12)
+    ori4 = doc.addVertex (-5, 0, 17)
+
+    vx   = doc.addVector ( 1 ,0, 0)
+    vz   = doc.addVector ( 0, 0, 1)
+
+    nl1 = 10
+    nl2 = 10
+
+    rsmall = 1
+    rmoy   = 2
+    rbig   = 3
+
+    cyl1  = doc.addCylinder (ori1, vz, rmoy,   nl1)
+    cyl2  = doc.addCylinder (ori2, vx, rsmall, nl2)
+
+    cyl3  = doc.addCylinder (ori3, vz, rmoy, nl1)
+    cyl4  = doc.addCylinder (ori4, vx, rbig, nl2)
+
+    grid1 = doc.makeCylinders (cyl1, cyl2)
+    grid2 = doc.makeCylinders (cyl4, cyl3)
+
+    nx_int = 0
+    nx_ext = 1
+    cyl_big   = 1
+    cyl_small = 0
+    S_E     = 0
+    S_NE    = 1
+    S_N     = 2
+    S_NW    = 3
+    S_MAXI  = 8
+                  ##    Cyl       i     j     k
+    qb = grid1. getQuadIJ (cyl_big,   nx_ext, S_E, 4)
+    qh = grid2. getQuadIJ (cyl_small, nx_ext, S_N, 0) 
+
+    vb0 = grid1.getVertexIJK (cyl_big,   2, S_E,  4)  ## cible
+    vb1 = grid1.getVertexIJK (cyl_big,   2, S_NE, 4)
+    vh0 = grid2.getVertexIJK (cyl_small, 2, S_N,  0)   ## depart
+    vh1 = grid2.getVertexIJK (cyl_small, 2, S_NW, 0)
+
+    hliste = []
+
+    hliste.append (qh)
+    for ny in range (S_MAXI) :
+        if ny != S_N :
+          hliste.append (grid2.getQuadIJ (cyl_small, nx_ext, ny, 0)) 
+
+    for ny in range (4) :
+        hliste.append (grid2.getQuadIJ (cyl_small, nx_int, ny, 0)) 
+
+    hauteur = 3
+    doc.joinQuads  (hliste, qb, vh0, vb0, vh1, vb1, hauteur)
+
+    doc.saveVtk ("lorraine.vtk")
+    return doc
+
+# ================================================================= Begin
+
+doc = test_double_t  ()
+
+law = doc.addLaw("Uniform", 4)
+
+for j in range(doc.countPropagation()):
+    propa = doc.getPropagation(j)
+    propa.setLaw(law) 
+
+mesh_hexas = hexablock.mesh("maillage:hexas", doc)
+
---
+++ src/TEST_PY/test_unit/test_transfo.py
@@ -0,0 +1,106 @@
+# -*- coding: latin-1 -*-
+# Hexa : Creation d'hexaedres 
+
+import hexablock
+import os
+import geompy
+
+#---+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8
+
+
+# ======================================================= make_grid
+def make_grid (doc) :
+
+    ori  = doc.addVertex ( 0, 0, 0)
+    vz   = doc.addVector ( 0, 0, 1)
+    vx   = doc.addVector ( 1 ,0, 0)
+
+    dr = 1
+    da = 360
+    dl = 1
+
+    nr = 1
+    na = 6
+    nl = 1
+    grid = doc.makeCylindrical (ori, vx,vz, dr,da,dl, nr,na,nl, False)
+    doc .saveVtk ("transfo1.vtk")
+    return grid
+
+# ======================================================= test_translation
+def test_translation () :
+
+    doc  = hexablock.addDocument()
+    grid = make_grid (doc)
+
+    devant = doc.addVector (10, 0, 0)
+    grid2  = doc.makeTranslation (grid, devant)
+
+    doc .saveVtk ("transfo2.vtk")
+    return doc
+
+# ======================================================= test_scale
+def test_scale () :
+
+    doc  = hexablock.addDocument()
+    grid = make_grid (doc)
+
+    dest   = doc.addVertex (15, 0, 0)
+    grid2  = doc.makeScale (grid, dest, 0.5)
+
+    doc .saveVtk ("transfo3.vtk")
+    return doc
+
+# ======================================================= test_sym_point
+def test_sym_point () :
+
+    doc  = hexablock.addDocument()
+    grid = make_grid (doc)
+
+    orig  = doc.addVertex (5, 0, 0)
+    grid2 = doc.makeSymmetryPoint (grid, orig)
+
+    doc .saveVtk ("transfo4.vtk")
+    return doc
+
+# ======================================================= test_sym_line
+def test_sym_line () :
+
+    doc  = hexablock.addDocument()
+    grid = make_grid (doc)
+
+    orig  = doc.addVertex (5, 0, 0)
+    dir   = doc.addVector (0, 0, 1);
+    grid2 = doc.makeSymmetryLine (grid, orig, dir)
+
+    doc .saveVtk ("transfo5.vtk")
+    return doc
+
+# ======================================================= test_sym_plan
+def test_sym_plan () :
+
+    doc  = hexablock.addDocument()
+    grid = make_grid (doc)
+
+    orig  = doc.addVertex (5, 0, 0)
+    dir   = doc.addVector (1, 0, 0);
+    grid2 = doc.makeSymmetryPlane (grid, orig, dir)
+
+    doc .saveVtk ("transfo6.vtk")
+    return doc
+
+# ================================================================= Begin
+
+### doc = test_translation  ()
+doc = test_scale  ()
+### doc = test_sym_point  ()
+### doc = test_sym_line  ()
+### doc = test_sym_plan  ()
+
+law = doc.addLaw("Uniform", 4)
+
+for j in range(doc.countPropagation()):
+    propa = doc.getPropagation(j)
+    propa.setLaw(law) 
+
+mesh_hexas = hexablock.mesh("maillage:hexas", doc)
+
