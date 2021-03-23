8bf9123643544b132f72f14e5ab5141c897b3c3b
vsr <vsr@opencascade.com>
2021-03-23 21:41:34 +0300

    Copyright update 2021

--- src/HEXABLOCK/HexDocument_tools.cxx
+++ src/HEXABLOCK/HexDocument_tools.cxx
@@ -1,7 +1,7 @@
 
 // C++ : Classe Document : Methodes internes 2011
 
-// Copyright (C) 2009-2020  CEA/DEN, EDF R&D
+// Copyright (C) 2009-2021  CEA/DEN, EDF R&D
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
