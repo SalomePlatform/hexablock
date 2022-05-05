b807e66955ae476b30f59bc29f1fe6c813837063
vsr <vsr@opencascade.com>
2022-05-05 17:00:48 +0300

    Copyright update 2022

--- src/HEXABLOCK/HexGlobale.hxx
+++ src/HEXABLOCK/HexGlobale.hxx
@@ -1,7 +1,7 @@
 
 // Singleton : Var globales : Disposition sommets/aretes/faces d'un hexaedre
 
-// Copyright (C) 2009-2021  CEA/DEN, EDF R&D
+// Copyright (C) 2009-2022  CEA/DEN, EDF R&D
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
--- src/TEST_PY/test_v6/test_cone.py
+++ src/TEST_PY/test_v6/test_cone.py
@@ -1,7 +1,7 @@
 # -*- coding: latin-1 -*-
 # Hexa : Creation d'hexaedres 
 
-# Copyright (C) 2009-2021  CEA/DEN, EDF R&D
+# Copyright (C) 2009-2022  CEA/DEN, EDF R&D
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
