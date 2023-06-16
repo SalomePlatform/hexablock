359245fea46fbec5630a9a72bfb2d906261fa36f
mbs <martin.bernhard@opencascade.com>
2023-06-16 19:51:49 +0100

    updated copyright message

--- src/HEXABLOCK/HexWitness.cxx
+++ src/HEXABLOCK/HexWitness.cxx
@@ -1,7 +1,7 @@
 
 // C++ : Dump python
 
-// Copyright (C) 2009-2022  CEA/DEN, EDF R&D
+// Copyright (C) 2009-2023  CEA/DEN, EDF R&D
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
